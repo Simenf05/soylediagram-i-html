08ac436c49b919cbc24a088485a8ea0d794de13d
John Doe <johndoe@example.com>
2023-11-07 10:01:38 +0100

    years er lagt til men ikke noe soyle

---
+++ script.js
@@ -0,0 +1,70 @@
+
+
+const fileInputEl = document.querySelector("#fileInput")
+const soylerEl = document.querySelector("#soyler")
+
+
+function makeSoyle(data) {
+
+    const year = data[0]
+
+    const boys = Number(data[1])
+    const girls = Number(data[2])
+
+    const sum = data[3]
+
+    const soyleEl = document.createElement("div")
+    soyleEl.className = "soyle"
+
+    soyleEl.innerText = year
+
+
+    soylerEl.appendChild(soyleEl)
+
+
+}
+
+fileInputEl.onchange = () => {
+    
+
+    const file = fileInputEl.files[0]
+    const fr = new FileReader()
+    
+
+    fr.onload = () => {
+
+        const data = []
+        const linesArr = fr.result.split(/\r?\n/)
+        
+        linesArr.forEach(line => {
+            data.push(line.split(";"))
+        })
+
+        
+        data[0].forEach((value, i) => {
+
+            if (i === 0) {
+
+                return
+
+            }
+            else {
+
+                const individualData = []
+
+                data.forEach(line => {
+                    individualData.push(line[i])
+                });
+
+                makeSoyle(individualData)
+
+
+
+            }
+        })
+
+    }
+
+    fr.readAsText(file)
+}
+
